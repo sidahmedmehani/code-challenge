a602c3ba87c10960f37f64d80a5c0c37b6e9547d
sidahmed mehani <sidahmedmehani@sidahmeds-MBP.fritz.box>
2021-07-16 13:48:08 +0200

    make the api call inside useEffect hook

--- src/components/ShortenUrlForm.jsx
+++ src/components/ShortenUrlForm.jsx
@@ -1,31 +1,41 @@
 /* eslint no-unused-vars: 1 */
-import React, { useCallback, useState } from 'react';
-import useIsMounted from '../useIsMounted';
+import React, { useCallback, useEffect, useState } from 'react';
 import { bitlyApi } from '../utils';
 
 const ShortenUrlForm = () => {
     const [value, setValue] = useState('');
     const [shortUrl, setShortUrl] = useState('');
-    const isMounted = useIsMounted();
+    const [isSubmited, setIsSubmited] = useState(false);
     const onChange = useCallback((e) => {
         setValue(e.target.value);
     }, []);
 
-    const onSubmit = useCallback(async (e) => {
+    const onSubmit = useCallback((e) => {
         e.preventDefault();
-        const response = await bitlyApi(value);
-        setValue('');
-        await navigator.clipboard.writeText(response.data.link);
-        if (isMounted) {
-            setShortUrl(response.data.link);
-        }
+        setIsSubmited(true);
+    }, []);
 
-    }, [value]);
+    useEffect(async () => {
+        if (isSubmited) {
+            const response = await bitlyApi(value);
+            setValue('');
+            await navigator.clipboard.writeText(response.data.link);
+            setShortUrl(response.data.link);
+            setIsSubmited(false);
+        }
+    }, [isSubmited]);
+
     return (
         <form onSubmit={onSubmit}>
             <label htmlFor="shorten">
                 Url:
-                <input placeholder="Url to shorten" id="shorten" type="text" value={value} onChange={onChange} />
+                <input
+                    placeholder="Url to shorten"
+                    id="shorten"
+                    type="text"
+                    value={value}
+                    onChange={onChange}
+                />
             </label>
             <input type="submit" value="Shorten and copy URL" />
             {/* TODO: show below only when the url has been shortened and copied */}
